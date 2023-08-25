4fb1ea28a85075ff7ea36037aadeef144dbdb430
Jonathas Rodrigues <jonathasrc96@gmail.com>
2023-08-24 23:45:39 -0300

    Instrução pratica 1: exercicio 6

--- programacao_imperativa/Projeto1/meuprog2.cpp
+++ programacao_imperativa/Projeto1/meuprog2.cpp
@@ -1,9 +1,14 @@
-# include <iostream>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
 using namespace std;
 
 
 int main(void){
     float a, b;
+
+    float soma, subtracao, multiplicacao, divisao;
 
     cout << "Por favor, informe dois numeros" << endl;
     cout << "Informe primeiro numero: ";
@@ -11,14 +16,20 @@ int main(void){
 
     cout << "Informe segundo numero: ";
     cin >> b;
+    
+    soma = (float) a + b;
+    subtracao = (float) a - b;
+    multiplicacao = (float) a * b;
+    divisao = (float) a / b;
 
-    cout << "Soma: " << a + b << endl;
-    cout << "Subtração: " << a - b << endl;
-    cout << "Multiplicação: " << a * b << endl;
-    cout << "Divisão: " << a / b << endl;
-    cout << "Resto: " << a % b << endl;
+    cout << "Soma: " << soma << endl;
+    cout << "Subtração: " << subtracao << endl;
+    cout << "Multiplicação: " << multiplicacao << endl;
+    // cout << "Divisão: " << std::setprecision(2) << divisao << endl;
+    cout << "Divisão: " <<  divisao << endl;
 
-    // cout << "Resto" << a % b << endl;
+    // style C
+    printf("%.1f", divisao);      
 
     return 0;
 }
